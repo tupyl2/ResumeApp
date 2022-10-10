44d877d73e0b4aef9976073debe8926d02f92ecd
Lyssa Tupy <lyssatupy@Lyssas-MacBook-Pro.local>
2022-10-10 14:35:04 -0500

    added button functionality

--- app/src/main/java/lyssa/tupy/MainActivity.kt
+++ app/src/main/java/lyssa/tupy/MainActivity.kt
@@ -1,11 +1,45 @@
 package lyssa.tupy
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var workHistory = findViewById<Button>(R.id.workHistoryButton)
+
+        workHistory.setOnClickListener{
+
+            var moveToWorkHistory = Intent(getApplicationContext(), WorkHistoryActivity::class.java)
+            startActivity(moveToWorkHistory)
+        }
+
+        var call = findViewById<Button>(R.id.callButton)
+
+        call.setOnClickListener {
+            println("Call")
+
+            var phoneUri = Uri.parse("tel:18002343456")
+            var callIntent = Intent(Intent.ACTION_DIAL, phoneUri)
+            startActivity(callIntent)
+        }
+
+        var email = findViewById<Button>(R.id.emailButton)
+
+        email.setOnClickListener {
+            println("Email")
+
+            var emailIntent = Intent(Intent.ACTION_SEND)
+            emailIntent.setType("plain/text")
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, ("someemail@gmail.com"))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Let's Connect!")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "I really enjoyed your resume...")
+            startActivity(emailIntent)
+        }
     }
 }
---
+++ app/src/main/java/lyssa/tupy/WorkHistoryActivity.kt
@@ -0,0 +1,11 @@
+package lyssa.tupy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work_history)
+    }
+}
